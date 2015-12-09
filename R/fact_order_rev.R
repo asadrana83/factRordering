fc362109deed1c83c2d672e06512f9c4b55f351e
arunmarria <arun.marria@gmail.com>
2015-12-09 14:02:44 -0800

    Document has been updated

--- R/fact_order_rev.R
+++ R/fact_order_rev.R
@@ -1,3 +1,11 @@
+#' Sorting factor levels in reverse order
+#'
+#' @param Factor
+#'
+#' @return Factor
+#' @export
+#' @examples
+#' fact_order_rev(factor(c("short", "long", "short", "medium", "medium")))
 fact_order_rev <- function(x){
   factor(x,levels = rev(levels(x)))
 }
